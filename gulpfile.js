eea799b6a4ac972522ce01684d1d8d1b89c05d65
YuuKohaku <heimaolee@gmail.com>
2016-01-19 23:44:02 +0600

    babel fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,30 @@ var minify = require('gulp-minify');
 
 gulp.task('default', ['json', 'js']);
 
-gulp.task("js", function () {
-    return gulp.src(["src/**/*.js"])
-        .pipe(babel({
-            blacklist: ['bluebirdCoroutines', 'regenerator']
-        }))
-        .pipe(gulp.dest("build"));
+gulp.task("js", function() {
+	return gulp.src(["src/**/*.js"])
+		.pipe(babel())
+		.pipe(gulp.dest("build"));
 });
 
-gulp.task("json", function () {
-    return gulp.src(["src/**/*.json"])
-        .pipe(gulp.dest("build"));
+gulp.task("json", function() {
+	return gulp.src(["src/**/*.json"])
+		.pipe(gulp.dest("build"));
 });
 
 gulp.task("dev", ['default'],
-    function () {
-        return gulp.src(["dev-util/**/*.js"])
-            .pipe(babel({
-                blacklist: ['bluebirdCoroutines', 'regenerator']
-            }))
-            .pipe(gulp.dest("build/dev-util"))
-            .on('end', function () {
-                require("./build/dev-util/app");
-            });
-    });
+	function() {
+		return gulp.src(["dev-util/**/*.js"])
+			.pipe(babel())
+			.pipe(gulp.dest("build/dev-util"))
+			.on('end', function() {
+				require("./build/dev-util/app");
+			});
+	});
 
 
-gulp.task('compress', function () {
-    gulp.src('dev-util/utils/jsonld.js')
-        .pipe(minify({}))
-        .pipe(gulp.dest('build/compressed'))
+gulp.task('compress', function() {
+	gulp.src('dev-util/utils/jsonld.js')
+		.pipe(minify({}))
+		.pipe(gulp.dest('build/compressed'))
 });
